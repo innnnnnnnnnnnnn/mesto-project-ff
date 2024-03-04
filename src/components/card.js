e1f9b408562ab41701cbe8a9c1488982ca3e6260
inna <inna.poyatcova@gmail.com>
2024-03-04 22:21:25 +0700

    готово

--- src/components/card.js
+++ src/components/card.js
@@ -1,27 +1,31 @@
-export function getCard(item, deleteCard, cardLike,OpenImageCard) {
+export function getCard(item, deleteCard, cardLike, OpenImageCard) {
   const card = document.querySelector("#card-template").content;
   const cardElement = card.querySelector(".card").cloneNode(true);
   const cardImage = cardElement.querySelector(".card__image");
   const cardTitle = cardElement.querySelector(".card__title");
-  const likeButton = cardElement.querySelector('.card__like-button')
-  likeButton.addEventListener('.click', cardLike)
+  const likeButton = cardElement.querySelector(".card__like-button");
+  likeButton.addEventListener(".click", cardLike);
   cardTitle.textContent = item.name;
   cardImage.src = item.link;
   cardImage.alt = item.name;
   cardElement
     .querySelector(".card__delete-button")
     .addEventListener("click", deleteCard);
-  cardElement.querySelector('.card__like-button').addEventListener('click', cardLike)
-  cardElement.querySelector('.card__image').addEventListener('click', OpenImageCard);
+  cardElement
+    .querySelector(".card__like-button")
+    .addEventListener("click", cardLike);
+  cardElement
+    .querySelector(".card__image")
+    .addEventListener("click", OpenImageCard);
   return cardElement;
 }
-  
-  //Удаление карточки
-  export function deleteCard(event) {
-    event.target.closest(".card").remove();
-  }
-  
-  //функция лайка 
-  export function cardLike(event){
-    event.target.classList.toggle('card__like-button_is-active')
-  }
+
+//Удаление карточки
+export function deleteCard(event) {
+  event.target.closest(".card").remove();
+}
+
+//функция лайка
+export function cardLike(event) {
+  event.target.classList.toggle("card__like-button_is-active");
+}
--- src/components/modal.js
+++ src/components/modal.js
@@ -1,43 +1,39 @@
+//Функция открытия попап
+export function openPopup(popup) {
+  popup.classList.add("popup_is-opened", "popup_is-animated");
+  document.addEventListener("keydown", handleEscClose);
+  popup.addEventListener("click", handleOverlayClose);
+  popup.addEventListener("click", handleButtonClose);
+}
 
-  
-  //Функция открытия попап
-  export function openPopup(popup) {
-    popup.classList.add('popup_is-opened', 'popup_is-animated');
-    document.addEventListener('keydown', handleEscClose);
-      popup.addEventListener('click', handleOverlayClose);
-      popup.addEventListener('click', handleButtonClose);
-    
+//функция закрытия попап
+export function closePopup(popup) {
+  popup.classList.remove("popup_is-opened");
+  document.removeEventListener("keydown", handleEscClose);
+  popup.removeEventListener("click", handleOverlayClose);
+  popup.removeEventListener("click", handleButtonClose);
+}
+
+//Закрытие попапов по клику на крестик
+function handleButtonClose(evt) {
+  if (evt.target.classList.contains("popup__close")) {
+    const popup = document.querySelector(".popup_is-opened");
+    closePopup(popup);
   }
-  
-  //функция закрытия попап
-  export function closePopup(popup){
-      popup.classList.remove('popup_is-opened')
-      document.removeEventListener('keydown', handleEscClose);
-      popup.removeEventListener('click', handleOverlayClose);
-      popup.removeEventListener('click', handleButtonClose);
+}
+
+//Функция закрывает модальное окно при нажатии клавиши Escape
+function handleEscClose(evt) {
+  if (evt.key === "Escape") {
+    const popup = document.querySelector(".popup_is-opened");
+    closePopup(popup);
   }
-  
-  //Закрытие попапов по клику на крестик
-  function handleButtonClose(evt) {
-    if (evt.target.classList.contains('popup__close')) {
-        const popup = document.querySelector('.popup_is-opened');
-       closePopup(popup);
-    }
+}
+
+//Закрытие попапов по клику на оверлей
+function handleOverlayClose(evt) {
+  if (!evt.target.closest(".popup__content")) {
+    const popup = document.querySelector(".popup_is-opened");
+    closePopup(popup);
   }
-  
-  
-  //Функция закрывает модальное окно при нажатии клавиши Escape
-  function handleEscClose(evt) {
-      if (evt.key === 'Escape') {
-        const popup = document.querySelector('.popup_is-opened');
-          closePopup(popup);
-      }
-  }
-  
-  //Закрытие попапов по клику на оверлей
-  function handleOverlayClose(evt) {
-    if (!evt.target.closest('.popup__content')) {
-        const popup = document.querySelector('.popup_is-opened');
-        closePopup(popup);
-    }
-  }
+}
